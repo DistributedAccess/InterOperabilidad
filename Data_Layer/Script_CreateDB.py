b9ad2232d971fad60fb49fef7280f94d8112ee1d
Parzival <echeverri.va@gmail.com>
2018-09-30 17:15:56 -0500

    Create BD script listo!

--- Data_Layer/Script_CreateDB.py
+++ Data_Layer/Script_CreateDB.py
@@ -1,8 +1,26 @@
 import sys
 sys.path.append("..")
+from Infrastructure.Config import Config
 from Infrastructure.Logging import Logging
+from Interop_Create_DB import Interop_Create_DB
 
 """         Esta clase se encarga unicamente de llamar la clase
         Interop_Create_DB para crear la base de datos y sus respectivas
         tablas, ademas de ingresar el usuario de configuracion...
 """
+
+user   = config['MySql']['User']
+passwd = config['MySql']['Password']
+host   = config['MySql']['Host']
+db     = config['MySql']['DataBase']
+
+if __name__ == '__main__':
+    BD = Interop_Create_DB(user, passwd, host, db)
+    BD.Create_DataBase()
+    BD.Conectar_Base()
+    BD.Create_Usuario()
+    BD.Create_Horario()
+    BD.Create_Bitacora()
+    BD.Create_Usuario_Horario()
+    BD.Create_Usuario_Servidor()
+    BD.Cerrar_Conexion()
